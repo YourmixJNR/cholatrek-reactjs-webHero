897dd78e6bba0720a76170c4eba70923f7257a91
Michael Victor <radmakson@gmail.com>
2023-10-08 00:11:38 +0100

    Hero Section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from "./Components/header";
+import Hero from "./Components/hero";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Hero />
     </div>
   );
 }
---
+++ src/Components/hero.jsx
@@ -0,0 +1,25 @@
+import '../App.css'
+
+const Hero = () => {
+    return(
+        <hero>
+
+            <div>
+                <img 
+                src="hero.webp" 
+                alt="Hero Image" 
+                />
+            </div>
+
+            <div className='hero-word'>
+                <i>60% Discount</i>
+                <h1>Winter Collecton</h1>
+                <em>Best Cloth Collection By 2020!</em>
+                <button>Shop Now</button>
+            </div>
+
+        </hero>
+    )
+}
+
+export default Hero;
