fc9f05e9b6d9f0e7969f1d950918e31406263ab2
Michael Victor <radmakson@gmail.com>
2023-10-07 23:24:51 +0100

    Header in progrss

--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -3,6 +3,26 @@ import '../App.css'
 const Header = () => {
     return (
         <header>
+
+            <div className='nav-bar'>
+                <div className='logo'>
+                    <img
+                    src="logo.webp"
+                    alt="Website Logo"
+                    className="logo"
+                    />
+                </div>
+            </div>
+
+            <div className='links'>
+                <ul>
+                    <li>Home</li>
+                    <li>Category</li>
+                    <li>Latest</li>
+                    <li>Blog</li>
+                    <li>Pages</li>
+                </ul>
+            </div>
             
         </header>
     );
