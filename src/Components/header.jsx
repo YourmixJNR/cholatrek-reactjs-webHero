a350f86b1c9042774592b48501764cc0e39c96a2
Michael Victor <radmakson@gmail.com>
2023-10-07 23:36:12 +0100

    styling

--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -5,6 +5,7 @@ const Header = () => {
         <header>
 
             <div className='nav-bar'>
+
                 <div className='logo'>
                     <img
                     src="logo.webp"
@@ -12,33 +13,34 @@ const Header = () => {
                     className="logo"
                     />
                 </div>
-            </div>
 
-            <div className='links'>
-                <ul>
-                    <li>Home</li>
-                    <li>Category</li>
-                    <li>Latest</li>
-                    <li>Blog</li>
-                    <li>Pages</li>
-                </ul>
-            </div>
+                <div className='links'>
+                    <ul>
+                        <li>Home</li>
+                        <li>Category</li>
+                        <li>Latest</li>
+                        <li>Blog</li>
+                        <li>Pages</li>
+                    </ul>
+                </div>
 
-            <div className='search'>
-                <input
-                type="text"
-                placeholder='Search Product'
-                />
-            </div>
+                <div className='search'>
+                    <input
+                    type="text"
+                    placeholder='Search Product'
+                    />
+                </div>
 
-            <div className="sign">
-                <button>Sign In</button>
-            </div>
+                <div className="sign">
+                    <button>Sign In</button>
+                </div>
 
-            <div className='drop-down'>
-                <img
-                src="./images/drop-down.svg"
-                alt="" />
+                <div className='drop-down'>
+                    <img
+                    src="./images/drop-down.svg"
+                    alt="" />
+                </div>
+
             </div>
 
         </header>
